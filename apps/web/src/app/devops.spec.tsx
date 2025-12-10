e8113fcb8480a294ad5822bc220663f684699469
Nicole Souza <nicolemadeleines@gmail.com>
2025-12-09 21:10:17 -0300

    feat: habilita pipeline completa com testes e build

---
+++ apps/web/src/app/devops.spec.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+
+describe('DevOps Check', () => {
+  it('deve garantir que 1 + 1 é igual a 2 (Teste de Sanidade)', () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
